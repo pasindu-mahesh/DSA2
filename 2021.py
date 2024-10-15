6e80f5a8d156dea2413ac16d1a7240cb84401719
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2024-10-16 00:01:18 +0530

    new

---
+++ 2021.py
@@ -0,0 +1,16 @@
+def func(num):
+    if num == 1:
+        return 1
+    else:
+        return num + (func(num -1))
+
+while True:
+    num = int(input("Entre numbers :"))
+
+    if num == -1:
+        print("Finished!")
+        break
+
+    else:
+        
+        print("output : ",func(num))
